363a646a3dad1fb914f1320aa93ab3076cef8260
rosskush <ross.kush@gmail.com>
2018-06-15 09:50:56 -0500

    added the nad83 crs and the reporjection to nad83 albers, do not know if this is the right one though so might need to change the second proj4 line

--- test_scripts/creates_RG_shp.py
+++ test_scripts/creates_RG_shp.py
@@ -13,7 +13,13 @@ masterDF['geometry'] = masterDF.apply(lambda xy: Point(xy['DEC_LONG_VA'],xy['DEC
 masterDF = masterDF[['geometry', 'DRAIN_AREA_VA', 'CONTRIB_DRAIN_AREA_VA', 'STATION_NM', 'SITE_NO']]
 # masterDF['geometry'] = masterDF['geometry'].astype(str)
 # masterDF['DEC_LAT_VA'] = masterDF['DEC_LAT_VA'].astype(float)
-masterDF = gpd.GeoDataFrame(masterDF,geometry='geometry') #,crs={'init':'epsg:4269'})
+
+proj4 = '+proj=longlat +ellps=GRS80 +datum=NAD83 +no_defs ' # from epsg 4269
+masterDF = gpd.GeoDataFrame(masterDF,geometry='geometry' ,crs=proj4)
 # print(masterDF.head())
+
+# now reporject to nad83 albers
+proj4_albers = '+proj=aea +lat_1=27.5 +lat_2=35 +lat_0=18 +lon_0=-100 +x_0=1500000 +y_0=6000000 +ellps=GRS80 +datum=NAD83 +units=m +no_defs ' # i'm guessing it is this one http://spatialreference.org/ref/epsg/3083/ but double check
+masterDF.to_crs(proj4_albers)
 print(masterDF.dtypes())
 masterDF.to_file(os.path.join('output', 'shapefiles', 'RioGrandeSites.shp'))
